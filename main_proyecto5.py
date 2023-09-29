e2cd58d9354a96744344366a86d50695c23864bb
santy <santiagogamboa988@gmail.com>
2023-09-28 20:39:57 -0500

    proyecto #5 final

--- main_proyecto5.py
+++ main_proyecto5.py
@@ -1,5 +1,7 @@
 import os
 import random
+import readchar
+from readchar import key
 
 class Juego:
     def __init__(self, nb_jugador, mapa, posicion_inicial, posicion_final):
@@ -22,92 +24,93 @@ class Juego:
     def mostrar_laberinto(self):
         self.limpiar_pantalla()
         for fila in self.mapa:
-            print(''.join(fila))
+            fila_sin_coordenadas = ' '.join(['.' if c.isdigit() else c for c in fila])
+            print(fila_sin_coordenadas)
         print("Presiona 'q' en cualquier momento para salir del programa.")
 
     def main_loop(self):
         px, py = self.posicion_inicial
-        
-        while (px, py) != self.posicion_final:
+        mapa_alto = len(self.mapa)
+        mapa_ancho = len(self.mapa[0])
+
+        while True:
             self.mapa[py][px] = 'P'
             self.mostrar_laberinto()
-            
-            tecla = input("Presiona una tecla: ")
+            tecla = readchar.readkey()
 
             if tecla == 'q':
                 print(f"Saliendo del programa, {self.nb_jugador}...")
-                break
+                return
 
-            if tecla == 'w':
+            if tecla == key.UP:
                 if py > 0 and self.mapa[py - 1][px] != '#':
                     self.mapa[py][px] = '.'
                     py -= 1
-            elif tecla == 's':
-                if py < len(self.mapa) - 1 and self.mapa[py + 1][px] != '#':
+            elif tecla == key.DOWN:
+                if py < mapa_alto - 1 and self.mapa[py + 1][px] != '#':
                     self.mapa[py][px] = '.'
                     py += 1
-            elif tecla == 'a':
+            elif tecla == key.LEFT:
                 if px > 0 and self.mapa[py][px - 1] != '#':
                     self.mapa[py][px] = '.'
                     px -= 1
-            elif tecla == 'd':
-                if px < len(self.mapa[0]) - 1 and self.mapa[py][px + 1] != '#':
+            elif tecla == key.RIGHT:
+                if px < mapa_ancho - 1 and self.mapa[py][px + 1] != '#':
                     self.mapa[py][px] = '.'
                     px += 1
 
-        print("El juego ha terminado. ¡Gracias por jugar!")
+            if (px, py) == self.posicion_final:
+                print("¡Ganaste!")
+                self.mostrar_laberinto()
+                return
 
 class JuegoArchivo(Juego):
-    def __init__(self, nb_jugador, mapa_folder):
+    def __init__(self, nb_jugador, mapa_folder, posicion_inicial, posicion_final):
         self.mapa_folder = mapa_folder
-        super().__init__(nb_jugador, [], (0, 0), (0, 0))  
+        super().__init__(nb_jugador, [], posicion_inicial, posicion_final)
         self.cargar_mapa_aleatorio()
 
     def cargar_mapa_aleatorio(self):
         archivos = os.listdir(self.mapa_folder)
         if not archivos:
             raise FileNotFoundError("No se encontraron mapas en la carpeta especificada.")
-
         nombre_archivo = random.choice(archivos)
         path_completo = os.path.join(self.mapa_folder, nombre_archivo)
-
         with open(path_completo, "r") as archivo_mapa:
             mapa_str = archivo_mapa.read()
             self.mapa = self.cadena_a_matriz(mapa_str)
-
             inicio, fin = self.encontrar_inicio_y_fin(mapa_str)
             self.posicion_inicial = inicio
             self.posicion_final = fin
 
     def encontrar_inicio_y_fin(self, mapa_str):
         filas = mapa_str.strip().split('\n')
-        inicio = None
-        fin = None
-
-        for y, fila in enumerate(filas):
-            for x, caracter in enumerate(fila):
-                if caracter == 'P':
-                    inicio = (x, y)
-                elif caracter == '.':
-                    fin = (x, y)
+        inicio = (0, 0)
+        fin = (len(filas[0]) - 1, len(filas) - 1)
 
         if inicio is None or fin is None:
             raise ValueError("El mapa no contiene coordenadas de inicio o fin válidas.")
-
         return inicio, fin
 
 if __name__ == "__main__":
     nb_jugador = input("HOLA BIENVENIDO, por favor ingresa tu nombre: ")
     print(f"BIEN HECHO, {nb_jugador}")
 
-    respuesta = input("¿Estás listo para jugar? (responde 'si' o 'no'): ").lower()
+    while True:
+        respuesta = input("¿Estás listo para jugar? (responde 'si' o 'no'): ").lower()
+        if respuesta == "si":
+            break
+        elif respuesta == "no":
+            print(f"Oh, tal vez en otro momento, {nb_jugador}. ¡Hasta luego!")
+            exit()
+        else:
+            print("Respuesta no válida. Por favor, responde 'si' o 'no'.")
 
-    if respuesta == "si":
-        print("¡Genial! Comencemos el juego.")
-        mapa_folder = "C:/Users/USUARIO/Desktop/juego/mapas"
-        juego = JuegoArchivo(nb_jugador, mapa_folder)
-        juego.main_loop()
-    elif respuesta == "no":
-        print(f"Oh, tal vez en otro momento, {nb_jugador}. ¡Hasta luego!")
-    else:
-        print("No comprendo tu respuesta. Por favor, responde 'si' o 'no'.")
+    mapa_folder = "mapas/"
+
+    posicion_inicial_mapa1 = (0, 0)
+    posicion_final_mapa1 = (20, 20)
+    juego_mapa1 = JuegoArchivo(nb_jugador, mapa_folder, posicion_inicial_mapa1, posicion_final_mapa1)
+    juego_mapa1.main_loop()
+
+    print(f"¡Felicidades, {nb_jugador}! Has completado el primer mapa.")
